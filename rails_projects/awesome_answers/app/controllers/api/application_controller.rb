a35840c25eaecb4c8aae77854f15e19f3b44fcb5
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-08-08 23:05:03 -0700

    Add rails_same_sit_cookie gem for cross-origin cookie sharing

--- rails_projects/awesome_answers/app/controllers/api/application_controller.rb
+++ rails_projects/awesome_answers/app/controllers/api/application_controller.rb
@@ -1,4 +1,14 @@
 class Api::ApplicationController < ApplicationController
+    #Rails expects that any POST request made to any of its controllers will include a CSRF token
+    #This is used to prevent cross-site request forgery attacks
+    #With our API, we expect to have our data and requests public, so no need for this token
+
+    # Use this controller to share methods among only the api controllers.
+
+    # When making POST, PATCH, and DELETE requests to controllers, 
+    # the rails authenticity token will be provided. This isn't
+    # needed for public HTTP apis, so we'll skip it.
+    
     skip_before_action :verify_authenticity_token
 
     private
