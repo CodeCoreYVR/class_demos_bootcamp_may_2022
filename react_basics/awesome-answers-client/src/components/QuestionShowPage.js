bbaefcff51c337ff1da1703b6c6547f3f3ad5c8c
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-08-10 09:32:52 -0700

    js react change QuestionShowPage to class component

--- react_basics/awesome-answers-client/src/components/QuestionShowPage.js
+++ react_basics/awesome-answers-client/src/components/QuestionShowPage.js
@@ -1,27 +1,31 @@
 import QuestionDetails from "./QuestionDetails";
 import AnswerList from "./AnswerList";
+import { Component } from 'react';
 
-const QuestionShowPage = () => {
-  return(
-    <div>
-      <QuestionDetails 
-        title="What is your favourite color?"
-        body="Red, Green, Blue, etc."
-        author={{ full_name: "Admin User" }}
-        created_at={new Date()}
-        view_count={100}
-      />
-
-      <AnswerList list={
-        [
-          {body: "red", author_name: "Jon", created_at: "2021-10-2"},
-          {body: "yellow", author_name: "Bob", created_at: "2022-1-2"},
-          {body: "blue", author_name: "Mike", created_at: "2022-3-24"},
-        ]
-      } />
-
-    </div>
-  )
+class QuestionShowPage extends Component {
+  render(){
+    return(
+      <div>
+        <QuestionDetails 
+          title="What is your favourite color?"
+          body="Red, Green, Blue, etc."
+          author={{ full_name: "Admin User" }}
+          created_at={new Date()}
+          view_count={100}
+        />
+  
+        <AnswerList list={
+          [
+            {body: "red", author_name: "Jon", created_at: "2021-10-2"},
+            {body: "yellow", author_name: "Bob", created_at: "2022-1-2"},
+            {body: "blue", author_name: "Mike", created_at: "2022-3-24"},
+          ]
+        } />
+  
+      </div>
+    )
+  }
 }
 
 export default QuestionShowPage
+
