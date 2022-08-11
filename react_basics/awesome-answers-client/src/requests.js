fff4dbf08ee40a01010d61212196a7e0d9668ab0
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-08-11 10:48:40 -0700

    js react http requests, hacky sign in, default port 5500

---
+++ react_basics/awesome-answers-client/src/requests.js
@@ -0,0 +1,60 @@
+//Base URL that we are going to send the requests to (the API server)
+const baseURL = "http://localhost:3000/api/v1";
+
+//========Question========>
+export const Question = {
+    //Fetch all the questions from server
+    index(){
+        return fetch(`${baseURL}/questions`)
+        .then(response => {
+            console.log(response);
+            return response.json()
+        })
+    },
+    create(params){
+        return fetch(`${baseURL}/questions`, {
+            method: 'POST',
+            credentials: 'include', //need this for cookies
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        }).then((res) => res.json());
+    },
+    show(id){
+        return fetch(`${baseURL}/questions/${id}`)
+        .then(res => res.json());
+    },
+    update(id, params){
+        return fetch(`${baseURL}/questions/${id}`, {
+            method: 'PATCH',
+            credentials: 'include',
+            headers: {
+              "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(params)
+        }).then(res => res.json())
+    },
+    destroy(id) {
+        return fetch(`${baseURL}/questions/${id}`, {
+          method: 'DELETE',
+          credentials: 'include',
+        })
+    }
+}
+
+//Sign In AJAX Helper
+export const Session = {
+    create(params){
+        return fetch(`${baseURL}/session`, {
+            method: 'POST',
+            credentials: 'include', //need for cookies to be allowed to be sent cross-origin
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        }).then(res => res.json());
+    }
+}
+
+
