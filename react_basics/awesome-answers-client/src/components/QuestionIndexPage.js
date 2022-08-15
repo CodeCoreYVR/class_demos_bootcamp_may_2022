8f8f16396156a3c490a33447a36fdc107f0362ae
Matt <mattcdneale@gmail.com>
2022-08-15 10:50:01 -0700

    react hooks refactor questionindexpage

--- react_basics/awesome-answers-client/src/components/QuestionIndexPage.js
+++ react_basics/awesome-answers-client/src/components/QuestionIndexPage.js
@@ -1,48 +1,39 @@
 import QuestionDetails from "./QuestionDetails";
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Question } from '../requests';
 import { Link } from 'react-router-dom';
 
-class QuestionIndexPage extends Component {
-    constructor(props){
-        super(props)
-        this.state = { questions: []}
-    }
+export default function QuestionIndexPage() {
+    const [questions, setQuestions] = useState([])
 
-    componentDidMount(){
+    useEffect(() => {
         Question.index()
         .then((questionsData) => {
-            this.setState((state) => {
-                return {
-                    questions: questionsData
-                }
-            })
+            setQuestions(questionsData)
         })
-    }
-    deleteQuestion(id){
+    }, [])
+    const deleteQuestion = (id) =>{
         console.log(id)
         //we can't reset the value of state in this way
         // this.state.questions = this.state.questions.filter(q => q.id != id)
         //you can only reset the value by using this.setState()
 
-        this.setState({
-            questions: this.state.questions.filter(q => q.id != id)
-        })
-    }
-
-    render(){
-        return(
-            <>
-                <ul>
-                    {
-                    this.state.questions.map((q,i) => {
-                        return <li key={i}>{q.id} - <Link to={`/questions/${q.id}`}>{q.title}</Link> <button onClick={() => { this.deleteQuestion(q.id) }}>Delete</button> </li>
-                    })  
-                    }
-                </ul>
-          </>
+        setQuestions(
+            questions.filter(q => q.id !== id)
         )
     }
+
+    return(
+        <>
+            <ul>
+                {
+                questions.map((q,i) => {
+                    return <li key={i}>{q.id} - <Link to={`/questions/${q.id}`}>{q.title}</Link> <button onClick={() => { deleteQuestion(q.id) }}>Delete</button> </li>
+                })  
+                }
+            </ul>
+        </>
+    )
 }
 
-export default QuestionIndexPage;
+
