34ae7c144ca39e99ecff7b01e7e3c1a5531c56f2
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-08-17 10:00:08 -0700

    rails api errors StandardError for generic errors

--- rails_projects/awesome_answers/app/controllers/api/application_controller.rb
+++ rails_projects/awesome_answers/app/controllers/api/application_controller.rb
@@ -11,6 +11,16 @@ class Api::ApplicationController < ApplicationController
     
     skip_before_action :verify_authenticity_token
 
+    #rescue_from PRIORITY
+    #The priority of rescue_from is in the reverse order of where the calls are made,
+    #meaning that the more specific errors should be rescued last and general errors
+    #should be rescued first
+
+    #The StandardError class is an ancestor of all the errors that programmers could possibly
+    #cause in their program. Rescuing from it will prevent nearly all errors from crashing the program
+    #NOTE: Use this very carefully and make sure to always log the error messages in some form
+    rescue_from StandardError, with: :standard_error
+
     #Error messaging handling
     #To send a json error message when user types in the wrong path
     #for example: localhost:3000/api/v1/somethingwrong
@@ -31,5 +41,23 @@ class Api::ApplicationController < ApplicationController
     private
     def authenticate_user!
         redirect_to new_session_path, notice: "Please sign in" unless user_signed_in?
+    end
+
+    protected
+
+    def standard_error(error)
+        logger.error error.full_message
+
+        render(
+            status: 500,
+            json: {
+                errors: [
+                    {
+                        type: error.class.to_s,
+                        message: error.message
+                    }
+                ]
+            }
+        )
     end
 end
